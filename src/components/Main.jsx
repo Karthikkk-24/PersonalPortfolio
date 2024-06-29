07380350cd53868930fac21e42aadce482cd6c3d
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-06-30 01:06:41 +0530

    Added Main, Navbar and Footer Components

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+export default function Main() {
+    return (
+        <div>
+            <Navbar />
+            <Footer />
+        </div>
+    );
+}
