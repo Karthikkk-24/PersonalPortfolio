6969d0cc5db254f75de71590e88ad0fe4f9a90da
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-06-30 01:13:12 +0530

    Changes to tailwind configuration file and changes to routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Main from './components/Main';
 
 function App() {
     return (
-        <>
+        <div className='bg-background w-screen min-h-screen'>
             <Router>
                 <Routes>
                     <Route path="/" Component={Main} />
                 </Routes>
             </Router>
-        </>
+        </div>
     );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors : {
+                "primary": "#bff500",
+                "background": "#121212"
+            }
+        },
     },
     plugins: [],
 }
