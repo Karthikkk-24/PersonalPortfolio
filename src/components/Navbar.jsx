0efb92eadf55ecba6e709b60daa718b5827d0e17
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-07-01 21:29:38 +0530

    Changes to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-
-
 export default function Navbar() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="w-screen h-24 border-b border-slate-50 flex items-center justify-center gap-5">
+            <a href="" className="text-white font-semibold">
+                Nav Item
+            </a>
+            <a href="" className="text-white font-semibold">
+                Nav Item
+            </a>
+            <a href="" className="text-white font-semibold">
+                Nav Item
+            </a>
+            <a href="" className="text-white font-semibold">
+                Nav Item
+            </a>
+            <a href="" className="text-white font-semibold">
+                Nav Item
+            </a>
+            <a href="" className="text-white font-semibold">
+                Nav Item
+            </a>
+        </div>
+    );
 }
