e3b8506d45d78fee76b5af5d0f3d6594bcc369b2
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-07-03 20:56:28 +0530

    Navbar Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
-import NavbarItem from "./NavbarItem";
+import DownloadSVG from './DownloadSVG';
+import NavbarItem from './NavbarItem';
 
 export default function Navbar() {
     return (
-        <div className="w-screen h-24 border-b border-slate-50 flex items-center justify-center gap-5">
-            <NavbarItem title="Home" link="/" />
-            <NavbarItem title="Projects" link="/projects" />
-            <NavbarItem title="Social" link="/socials" />
-            <NavbarItem title="Shop" link="/shop" />
-            <NavbarItem title="About" link="/about" />
-            <NavbarItem title="Contact Me" link="/contact" />
+        <div className="w-screen h-24 border-b border-slate-50 flex items-center justify-between gap-5 px-10">
+            <div className="w-48 h-full flex items-center justify-center">
+                <img src="/logo/1.png" className="h-full w-auto" alt="" />
+            </div>
+            <div className="w-auto flex items-center h-full justify-center gap-8">
+                <NavbarItem title="Home" link="/" />
+                <NavbarItem title="Projects" link="/projects" />
+                <NavbarItem title="Social" link="/socials" />
+                <NavbarItem title="Shop" link="/shop" />
+                <NavbarItem title="About" link="/about" />
+                <NavbarItem title="Contact Me" link="/contact" />
+            </div>
+            <div className="w-48 flex items-center justify-center">
+                <button className="w-auto h-auto px-5 py-3 flex gap-2 items-center justify-center bg-primary rounded-xl text-background">
+                    <DownloadSVG />
+                    Download CV
+                </button>
+            </div>
         </div>
     );
 }
--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 export default function NavbarItem({ title, link }) {
     return (
         <NavLink
-            className="text-primary font-semibold"
+            className="text-primary font-semibold after:w-0 hover:after:w-full after:h-0.5 after:bg-primary after:block after:transition-all after:duration-300"
             activeClassName="active"
             to={link}
         >
