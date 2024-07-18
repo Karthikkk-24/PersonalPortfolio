3e592c949f030a2c96957311b32ab53a42dda935
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-07-19 01:00:29 +0530

    Added other page Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
 import Main from './components/Main';
 import Projects from './components/Projects';
 import PublicRoute from './components/PublicRoute';
+import Shop from './components/Shop';
+import Social from './components/Social';
 
 function App() {
     return (
@@ -12,6 +16,10 @@ function App() {
                     <Route path="/" element={<PublicRoute />}>
                         <Route path="/" Component={Main} />
                         <Route path="/projects" Component={Projects} />
+                        <Route path='/socials' Component={Social} />
+                        <Route path='/contact' Component={Contact} />
+                        <Route path='/about' Component={About} />
+                        <Route path="/shop" Component={Shop} />
                     </Route>
                 </Routes>
             </Router>
