d1442a94ec631f6b03be0fd5d1b1a4f80c68432a
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-07-19 00:57:48 +0530

    Changes to navbar items

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -6,7 +6,7 @@ export default function NavbarItem({ title, link, active, onClick }) {
         <NavLink
             className={`${
                 active ? 'bg-slate-800' : ''
-            } text-primary font-semibold after:w-0 hover:after:w-full after:h-0.5 after:bg-primary after:block after:transition-all after:duration-300 w-full h-12 flex items-center justify-center rounded-lg`}
+            } text-primary font-semibold after:w-0 hover:after:w-full after:h-0.5 after:bg-primary after:block after:transition-all after:duration-300 w-full h-12 flex items-center justify-center rounded-lg relative after:absolute after:bottom-0`}
             to={link}
             onClick={onClick}
         >
