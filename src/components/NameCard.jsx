dbea18d48a2aa6a9e341412b24ec0acad4716453
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-05 23:46:44 +0530

    Changed tracking for words

--- src/components/NameCard.jsx
+++ src/components/NameCard.jsx
@@ -2,7 +2,7 @@ export default function NameCard() {
     return (
         <h1 className="w-full flex flex-col items-start justify-start text-9xl font-bebas text-white uppercase">
             <span className="font-bebas">I&apos;m Karthik</span>
-            <span className="font-bebas">Shettigar</span>
+            <span className="font-bebas tracking-wider">Shettigar</span>
         </h1>
     );
 }
