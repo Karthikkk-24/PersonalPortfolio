7a2bda4517565901a1b53985d05f5ee3e6eec2e5
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-06 00:31:06 +0530

    Added Navbar Component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,10 @@
 import NameCard from '../components/NameCard';
+import Navbar from '../components/Navbar';
 
 export default function Homepage() {
     return (
         <div className="w-full h-full flex items-center justify-center bg-background rounded-3xl p-10">
+            <Navbar />
             <NameCard />
         </div>
     );
