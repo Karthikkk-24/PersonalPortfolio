db2ab720862b23372a8ec74a86fbd2baaf255d64
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-05 22:34:15 +0530

    Added use location, and changes to public route and sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,39 @@
+import PropTypes from 'prop-types';
 import NavItem from './NavItem';
 
-export default function Sidebar() {
+export default function Sidebar({ currentPath }) {
     return (
         <div className="w-full h-full flex flex-col items-start justify-center p-10 gap-4 bg-background rounded-2xl">
-            <NavItem title="Home" path={'/'} active />
-            <NavItem title="Projects" path={'/projects'} />
-            <NavItem title="Blogs" path={'/blogs'} />
-            <NavItem title="Shop" path={'/shop'} />
-            <NavItem title="About" path={'/about'} />
-            <NavItem title="Contact" path={'/contact'} />
+            <NavItem title="Home" path={'/'} active={currentPath === '/'} />
+            <NavItem
+                title="Projects"
+                path={'/projects'}
+                active={currentPath === '/projects'}
+            />
+            <NavItem
+                title="Blogs"
+                path={'/blogs'}
+                active={currentPath === '/blogs'}
+            />
+            <NavItem
+                title="Shop"
+                path={'/shop'}
+                active={currentPath === '/shop'}
+            />
+            <NavItem
+                title="About"
+                path={'/about'}
+                active={currentPath === '/about'}
+            />
+            <NavItem
+                title="Contact"
+                path={'/contact'}
+                active={currentPath === '/contact'}
+            />
         </div>
     );
 }
+
+Sidebar.propTypes = {
+    currentPath: PropTypes.string.isRequired,
+};
