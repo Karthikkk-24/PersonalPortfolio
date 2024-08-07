4bae6ce8c6bd63a21ce9c3a6f77550f8fb7d65af
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-07 21:49:50 +0530

    Added Space Component and Changes to Homepage

---
+++ src/components/Space.jsx
@@ -0,0 +1,8 @@
+
+export default function Space() {
+  return (
+    <div className="w-full h-24">
+      
+    </div>
+  )
+}
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import NameCard from '../components/NameCard';
 import Navbar from '../components/Navbar';
+import Space from '../components/Space';
 
 export default function Homepage() {
     return (
         <div className="w-full h-full flex flex-col items-center justify-start bg-background rounded-3xl p-10">
             <Navbar />
             <NameCard />
+            <Space />
         </div>
     );
 }
