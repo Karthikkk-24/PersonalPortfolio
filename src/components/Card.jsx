b32b6a3da080261f74e4a4c1ab76157a2dbce156
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-08 08:50:55 +0530

    Projects and Card Changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export default function Card() {
   return (
-    <div>
+    <div className='w-full rounded-2xl shadow-lg hover:shadow-2xl hover:scale-105 transition-all'>
       
     </div>
   )
