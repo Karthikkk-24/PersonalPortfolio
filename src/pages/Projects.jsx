7bb05b78958dd5bc46f3e03a60c1daaa090f2096
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-08 08:50:57 +0530

    Projects and Card Changes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,11 @@
+import Card from '../components/Card';
+
 export default function Projects() {
     return (
         <div className="w-full h-full flex flex-col items-center justify-start bg-background rounded-3xl p-10">
             <div className="w-full h-auto grid grid-cols-3 place-items-center justify-items-center">
-
+                <Card />
             </div>
         </div>
-    )
+    );
 }
