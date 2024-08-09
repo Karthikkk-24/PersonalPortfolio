158673e70cb3265b9f95179b795bd0d07896dee9
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-09 21:16:09 +0530

    Modified Grid and Changed Card Styles

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,10 @@ import Card from '../components/Card';
 export default function Projects() {
     return (
         <div className="w-full h-full flex flex-col items-center justify-start bg-background rounded-3xl p-10">
-            <div className="w-full h-auto grid grid-cols-3 place-items-center justify-items-center">
+            <div className="w-full h-auto grid grid-cols-4 place-items-center justify-items-center gap-10">
+                <Card />
+                <Card />
+                <Card />
                 <Card />
             </div>
         </div>
