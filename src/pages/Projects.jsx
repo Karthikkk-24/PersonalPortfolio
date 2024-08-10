88c421269385d87a55e4932d7e91fff21ae72f43
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-10 07:09:44 +0530

    Changes to card and added hover animation adn popup for card image

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,7 +4,12 @@ export default function Projects() {
     return (
         <div className="w-full h-full flex flex-col items-center justify-start bg-background rounded-3xl p-10">
             <div className="w-full h-auto grid grid-cols-4 place-items-center justify-items-center gap-10">
-                <Card />
+                <Card
+                    image="/logo/1.png"
+                    title="Card Title"
+                    buttonText="Learn More"
+                    buttonUrl="/some-page"
+                />
                 <Card />
                 <Card />
                 <Card />
