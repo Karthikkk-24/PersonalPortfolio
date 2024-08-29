8f398a0103898c8bafc98389e7598e41f00f862b
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-29 20:07:30 +0530

    HomeScreen Layout Responsive

--- src/components/PublicRoute.jsx
+++ src/components/PublicRoute.jsx
@@ -6,10 +6,10 @@ const PublicRoute = () => {
     const location = useLocation();
 
     return (
-        <div className="w-full h-screen flex flex-col items-center justify-center">
+        <div className="w-full h-screen flex flex-col items-center justify-center max-md:h-auto">
             <div className="container h-full">
-                <div className="w-full h-full flex items-start justify-start py-16">
-                    <div className="w-96 h-full flex flex-col items-start justify-start p-2">
+                <div className="w-full h-full flex items-start justify-start py-16 max-md:flex-col max-md:py-5 max-md:px-3">
+                    <div className="w-96 h-full flex flex-col items-start justify-start p-2 max-md:w-full">
                         <Sidebar />
                     </div>
                     <div className="w-full h-full flex flex-col items-start justify-start p-2">
