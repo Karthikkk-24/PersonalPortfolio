0276ad50458b149321bf809a1a87a50eb4ee666d
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-09-03 19:26:08 +0530

    Projects Page Changes

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -448,40 +448,6 @@ export default function Blogs() {
     return (
         <div className="w-full h-full overflow-y-auto px-7 py-5 max-md:px-1">
             <div className="w-full h-auto grid grid-cols-3 place-items-center justify-items-center gap-10 max-md:grid-cols-1">
-                {/* {[
-                    {
-                        image: '/blogs/1.png',
-                        url: '/some-page',
-                        description: 'Card Description',
-                        title: 'Prompt Genie',
-                        date: '01.01.2022',
-                    },
-                    {
-                        image: '/blogs/2.png',
-                        url: '/some-page',
-                        description: 'Card Description',
-                        title: 'Prompt Genie',
-                        date: '01.01.2022',
-                    },
-                ].map((project, index) => (
-                    <div
-                        key={index}
-                        className={`transform transition-all duration-500 ease-out w-full ${
-                            isVisible
-                                ? 'scale-100 opacity-100'
-                                : 'scale-0 opacity-0'
-                        }`}
-                        style={{ transitionDelay: `${index * 100}ms` }}
-                    >
-                        <Card
-                            image={project.image}
-                            title={project.title}
-                            date={project.date}
-                            url={project.url}
-                            description={project.description}
-                        />
-                    </div>
-                ))} */}
                 {projects.reverse().map((project, index) => (
                     <div
                         key={index}
