11d7b0709ddf93ddfae503de93b924dc286957e4
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-09-05 23:37:29 +0530

    Added BoxItem Component and Added Title Sections For What I Do Component

--- src/components/BoxItem.jsx
+++ src/components/BoxItem.jsx
@@ -1,10 +1,19 @@
-export default function BoxItem() {
+import PropTypes from 'prop-types';
+
+export default function BoxItem({ title = '', icon = '' }) {
     return (
         <div className="rounded-2xl h-40 w-full neo-shadow flex items-center justify-start border-2 border-onyx p-5">
-            <div className="w-1/5 h-full flex items-center justify-center"></div>
-            <div className="w-4/5 h-full flex flex-col items-start justify-start gap-3">
-                <h1 className="text-3xl text-white font-bold">Web Design</h1>
+            <div className="w-1/5 h-full flex items-center justify-center">
+                {icon}
+            </div>
+            <div className="w-4/5 h-full flex flex-col items-start justify-center gap-3">
+                <h1 className="text-2xl text-white font-bold">{title}</h1>
             </div>
         </div>
     );
 }
+
+BoxItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.object,
+};
--- src/components/WhatIDo.jsx
+++ src/components/WhatIDo.jsx
@@ -1,7 +1,12 @@
+import BoxItem from "./BoxItem";
+
 export default function WhatIDo() {
     return (
         <div className="w-full grid h-auto grid-cols-2 gap-10">
-            
+            <BoxItem title="Front End Development" />
+            <BoxItem title="Backend End Development" />
+            <BoxItem title="Web Design" />
+            <BoxItem title="App Development" />
         </div>
     );
 }
