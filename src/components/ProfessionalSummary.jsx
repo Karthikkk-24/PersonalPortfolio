291f3a4a2f1695ee6b7d408893cd6eb6ab005b92
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-09-06 16:11:29 +0530

    Modified Personal Summary

--- src/components/ProfessionalSummary.jsx
+++ src/components/ProfessionalSummary.jsx
@@ -1,13 +1,22 @@
-
-
 export default function ProfessionalSummary() {
     return (
-        <p className="text-white text-base">
-            Experienced MERN Stack Developer with 3+ years of expertise in
-            building scalable web applications. Skilled in developing robust
-            backend systems, RESTful APIs, and responsive front-end interfaces.
-            Passionate about creating efficient, user-friendly solutions and
-            staying updated with the latest web technologies.
-        </p>
+        <div className="w-full h-auto flex flex-col items-start justify-start gap-4">
+            <p className="text-white text-base">
+                Experienced MERN Stack Developer with 3+ years of expertise in
+                building scalable web applications. Skilled in developing robust
+                backend systems, RESTful APIs, and responsive front-end
+                interfaces. Passionate about creating efficient, user-friendly
+                solutions and staying updated with the latest web technologies.
+            </p>
+            <p className="text-white text-base">
+                Proficient in both frontend and backend development, with a
+                strong focus on optimizing performance and enhancing user
+                experience. Experienced in working with cross-functional teams,
+                managing complex projects, and delivering high-quality code
+                under tight deadlines. Adept at problem-solving and continuously
+                improving applications through testing, debugging, and
+                integrating new features.
+            </p>
+        </div>
     );
 }
