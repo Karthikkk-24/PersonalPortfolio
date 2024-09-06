8ac76943a74ce7c59d42632b9049c99e0c8e4193
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-09-06 16:20:36 +0530

    What I DO Change and Box Item COmponent Changes

--- src/components/BoxItem.jsx
+++ src/components/BoxItem.jsx
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 
 export default function BoxItem({ title = '', icon = '' }) {
     return (
-        <div className="rounded-2xl h-40 w-full neo-shadow flex items-center justify-start border-2 border-onyx p-5">
-            <div className="w-1/5 h-full flex items-center justify-center">
-                {icon}
+        <div className="rounded-2xl h-32 w-full neo-shadow flex items-center justify-start border-2 border-onyx p-5 gap-6">
+            <div className="w-1/5 h-full flex items-center justify-center bg-slate-50 p-5 rounded-2xl">
+                <img src={icon} className='h-24 w-auto' alt="" />
             </div>
             <div className="w-4/5 h-full flex flex-col items-start justify-center gap-3">
                 <h1 className="text-2xl text-white font-bold">{title}</h1>
--- src/components/WhatIDo.jsx
+++ src/components/WhatIDo.jsx
@@ -3,10 +3,10 @@ import BoxItem from "./BoxItem";
 export default function WhatIDo() {
     return (
         <div className="w-full grid h-auto grid-cols-2 gap-10">
-            <BoxItem title="Front End Development" />
-            <BoxItem title="Backend End Development" />
-            <BoxItem title="Web Design" />
-            <BoxItem title="App Development" />
+            <BoxItem icon="/icons/frontend.svg" title="Front End Development" />
+            <BoxItem icon="/icons/backend.svg" title="Backend End Development" />
+            <BoxItem icon="/icons/web-design.svg" title="Web Design" />
+            <BoxItem icon="/icons/app.svg" title="App Development" />
         </div>
     );
 }
