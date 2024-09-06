3ba8cb168cbe099a8adb5a561775e4267e554a16
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-09-06 07:29:55 +0530

    Download Resume Option Added

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,33 @@
 import { MdDownload } from 'react-icons/md';
 import CallToAction from '../components/CallToAction';
-import CustomButton from '../components/CustomButton';
 import ProfessionalSummary from '../components/ProfessionalSummary';
 import WhatIDo from '../components/WhatIDo';
 
 export default function Homepage() {
+
+    const handleDownload = () => {
+        try {
+            const pdfUrl = '/resume/Karthik_Shettigar_Resume.pdf';
+            
+            const link = document.createElement('a');
+            link.href = pdfUrl;
+            link.download = 'Karthik_Shettigar_Resume.pdf';
+            document.body.appendChild(link);
+            
+            link.click();
+            
+            document.body.removeChild(link);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
     return (
         <div className="w-full h-full flex flex-col items-start justify-start gap-6">
             <ProfessionalSummary />
-            <CustomButton title="Download CV" icon={<MdDownload />} url="/download" />
+            <button className='w-auto h-10 bg-primary font-semibold text-background px-5 py-2 rounded-lg hover:scale-105 transition-all flex items-center justify-center gap-2' onClick={handleDownload}>
+                Download Resume <MdDownload />
+            </button>
             <WhatIDo />
             <CallToAction />
         </div>
